0100c352190dbd868c7cf098bfef9e5d45f06d31
calvinmen1988 <calvinmendoza88@gmail.com>
2017-11-16 00:00:54 -0500

    update skeleton

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,10 @@
 // begin JS with images and counters.
 $(document).ready(function() {
 
+    var counter = 0;
+    var wins = 0;
+    var losses =0;
+
 }
+
+//new game begins when counter reaches 0
